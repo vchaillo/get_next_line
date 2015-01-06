d5d0e6c0c127386d086bb2c577b49b2bd7dcf870
Valentin CHAILLOU <vchaillo@e3r11p1.42.fr>
2015-01-06 19:14:17 +0100

    retour au dernier commit

--- projet/main.c
+++ projet/main.c
@@ -6,7 +6,7 @@
 /*   By: vchaillo <vchaillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/22 08:31:21 by vchaillo          #+#    #+#             */
-/*   Updated: 2015/01/06 18:49:20 by vchaillo         ###   ########.fr       */
+/*   Updated: 2015/01/06 18:57:42 by vchaillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 int		main(int argc, char **argv)
 {
+	int		fd;
 	int		fd1;
 	int		fd2;
 	char	*line = NULL;
@@ -26,7 +27,7 @@ int		main(int argc, char **argv)
 		while (get_next_line(0, &line) == 1)
 			ft_putendl(line);
 	}
-	else
+	else if (argc == 3)
 	{
 		if ((fd1 = open(argv[1], O_RDONLY)) == -1)
 		{
@@ -55,6 +56,23 @@ int		main(int argc, char **argv)
 			if (get_next_line(fd2, &line) == 1)
 				ft_putendl(line);
 		}
+	}
+	else
+	{
+		if ((fd = open(argv[1], O_RDONLY)) == -1)
+		{
+			close(fd);
+			ft_putstr("Failed to open ");
+			ft_putstr(argv[1]);
+			ft_putendl(" file");
+			return (0);
+		}
+		else
+		{
+			while (get_next_line(fd, &line) == 1)
+				ft_putendl(line);
+		}
+	
 	}
 	return (0);
 }

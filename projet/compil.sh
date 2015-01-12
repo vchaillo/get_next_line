218b10b49619c9c909a48f9e70a34944c4afd7ac
Valentin CHAILLOU <vchaillo@e3r5p11.42.fr>
2015-01-12 19:20:06 +0100

    modif makefile et compil.sh

--- projet/compil.sh
+++ projet/compil.sh
@@ -3,4 +3,5 @@ make -C libft/
 gcc -Wall -Wextra -Werror -g -I libft/includes/ -c get_next_line.c
 gcc -Wall -Wextra -Werror -g -I libft/includes/ -c main.c
 gcc -g -o test_gnl get_next_line.o main.o -L libft/ -lft
+rm -f get_next_line.o main.o
 echo "\033[32mtest_gnl was created\033[0m"

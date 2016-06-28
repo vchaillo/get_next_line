1359c29b41f06045bd919c863ff04375afba2b14
vchaillo <valentinchaillou89@gmail.com>
2016-06-28 19:10:59 +0200

    rm gnl_checker + compil.sh

--- compil.sh
+++
@@ -1,7 +0,0 @@
-make -C libft/ fclean
-make -C libft/
-gcc -Wall -Wextra -Werror -g -I libft/include/ -c src/get_next_line.c
-gcc -Wall -Wextra -Werror -g -I libft/include/ -c src/main.c
-gcc -g -o test_gnl get_next_line.o main.o -L libft/ -lft
-rm -f get_next_line.o main.o
-echo "\033[32mtest_gnl was created\033[0m"
